d3d691b44e3455d9cd2df307ff532054bb6123ef
rafal1999 <ravstb4@gmail.com>
2020-10-21 07:59:48 +0200

    tabpix -> tabval

--- main.cpp
+++ main.cpp
@@ -1,82 +1,132 @@
 #include <iostream>
 #include <fstream>
+#include <cstdio>
 #include <dlfcn.h>
-
+#include <cmath>
 
 unsigned char header[54];
-char tabOfchars[]={' ','*','|','=','@'};//na razie 5 znaków 
-unsigned char * readBMP(const char * filname);
 
+unsigned char * readBmp(const char * filname, unsigned int & _w,unsigned int &  _h);
+void writeTxt(unsigned int **tab,const unsigned int & _w,const unsigned int &  _h);
+char returnChar (const unsigned int _v);
+//  -i input 
+//  -o output 
+//  -c bilioteka c
+//  -a biblioteka asm
 int main(int argc, char ** argv)
 {
   int txtImageWidht=100;
-  // int (*m)();
-  // void* handle = dlopen("libC.so", RTLD_LAZY);
-  // //double * p= new double(2.2);
-  // m=  (int (*)())dlsym(handle,"test");
-  //   if (!handle)
-  // {
-  //   return 1;
-  // }
   unsigned char *pImage; 
   unsigned char *p; 
-  pImage=readBMP("jeden .bmp");
+  unsigned int width;
+  unsigned int height;
+  unsigned int **tab;
+  tab=new unsigned int *[4];
+  *tab=new unsigned int [4];
+  // tab[0][0]=10;
+  pImage=readBmp("4na4.bmp",width,height);
+  if(pImage==nullptr)
+  {
+    return 1;
+  }
+  
   p=pImage;
   std::cout<<"p=ddd"<<(unsigned int)*p<<std::endl;
-  unsigned char **t=nullptr;
+  unsigned char **t=nullptr;//tablica BGR
   t=new  unsigned char* [4];//ilość komórek  wsk bedze zależna od ilości wątków 
   unsigned char ** pStartT = t;
-
+  std::cout<<std::endl<<width<<"--"<<height<<std::endl;
   *t=p;
   t++;
-  *t=p+(4*3);
+  *t=p+(width);
   t++;
-  *t=p+(8*3);
+  *t=p+(width*2);
 
   t++;
-  *t=p+(12*3);
-  t-=3;
-  *t+=3*2+6;//poruszasz się po ala 2 d tab
+  *t=p+(width*3);
+  // t-=3;
+  // *t+=3*2+6;//poruszasz się po ala 2 d tab
   for(int i=0;i<12*4;i++)
   {
     printf("%d-%i\n",i,*p);
     p++;
   }
-  for(int i=0;i<12;i++);
+ 
 
- printf("eeee%i",**t);
-  FILE *pF = fopen("d.bmp","w");
-  fwrite(header,sizeof(unsigned char),54,pF);
-  fwrite(pImage,sizeof(unsigned char),12*4,pF);
-  fclose(pF);
+  // printf("eeee%i",**t);
+  void (*test)(unsigned char **,unsigned int **,const unsigned int  ,const unsigned int  );
+  void* handle = dlopen("./libC.so", RTLD_LAZY);
+  std::cout<<dlerror();
+  if (!handle)
+  {
+    printf("handle error");
+    return 1;
+  }
+  test=(void (*)(unsigned char **,unsigned int **,const unsigned int  ,const unsigned int ))dlsym(handle,"convertPixToTabOfVal");
+  (*test)(pStartT,tab,12,4);
+  dlclose(handle);
+  
+  // fprintf(pF,"%3i",12);
+    
+  
+  // fwrite(header,sizeof(unsigned char),54,pF);
+  // fwrite(pImage,sizeof(unsigned char),12*4,pF);
   delete [] pImage;
   delete [] pStartT;
+  // writeTxt(tab,1,1);
+  //  for(int i=0;i<1;i++)
+     delete  [] *tab;
+  delete [] tab;
+  
   return 0;
 }
 
-unsigned char * readBMP(const char * _filname)
+unsigned char * readBmp(const char * _filname,unsigned int & _w,unsigned int &  _h)
 {
   FILE * pF=fopen(_filname,"rb");
   if(pF==nullptr)
   {
-    std::cout<<"bład"<<std::endl;
+    std::cout<<"bład w otwieraniu pliku:"<<_filname<<std::endl;
     return nullptr;
   }
   // unsigned char header[54];
   fread(header,sizeof(unsigned char),54,pF);
-  unsigned int width = *(int*)&header[18];
-  unsigned int height = *(int*)&header[22];
-  std::cout<<std::endl<<width<<":"<<height<<std::endl;
-  width*=3;
+  _w = *(int*)&header[18];
+  _h = *(int*)&header[22];
 
-  if(width % 4) //wyrównanie do 4
+  _w*=3;
+
+  if(_w % 4) //wyrównanie do 4
   {
     std::cout<<"wyrównanie!"<<std::endl;
-    width += 4 - (width % 4);
+    _w += 4 - (_w % 4);
   }
-  std::cout<<std::endl<<width<<":"<<height<<std::endl;
-  unsigned char *pP= new unsigned char [width*height];
-  fread(pP,sizeof(unsigned char),width*height,pF);
+  std::cout<<std::endl<<_w<<":"<<_h<<std::endl;
+  unsigned char *pP= new unsigned char [_w*_h];
+  fread(pP,sizeof(unsigned char),_w*_h,pF);
     fclose(pF);
   return pP;
+}
+void writeTxt(unsigned int **tab,const unsigned int & _w,const unsigned int &  _h)
+{
+  // c
+  FILE *pF = fopen("txt","w");
+  // for (int i = 0; i < 4; i++)
+  // {
+  //   for(unsigned int j=0;j<_w;j++)
+  //   {
+    
+  //   }
+  // }
+  fprintf(pF,"%i",tab[0][0]);
+  // fwrite(header,sizeof(unsigned char),54,pF);
+  // fwrite(pImage,sizeof(unsigned char),12*4,pF);
+  fclose(pF);
+}
+char returnChar(const unsigned int _v)
+{
+  char tabOfchars[]={' ','*','|','=','@'};//na razie 5 znaków 
+  if  (_v<=((pow(255,3))/3) && _v>=((pow(240,3))/3)) {return ' ';}
+  else {return '# ';} 
+  return 0; //tymaczasowo
 }
--- submain.c
+++ submain.c
@@ -1,8 +1,27 @@
 #include <stdio.h>
 
 
-int test() 
+void convertPixToTabOfVal(unsigned char ** _pPix,unsigned int **_tabVal,const unsigned int  _w,const unsigned int  _h) 
 {
-  printf("nanana");
-  return 0;
+
+  // printf("\ntest\n");
+  // for(int o=0;o<4;o++)
+  // {
+  //   for(int i=0;i<4;i++)
+  //   {
+  //     for(int j=0;j<3;j++)
+  //     {
+  //       _tabVal[i][o]+=**_pPix;
+  //       *_pPix++;
+  //     }
+  //     *_pPix++;
+  //   }
+  //   _pPix++;
+  // }
+  // return NULL;
+}
+
+void test ()
+{
+  printf("tse");
 }
